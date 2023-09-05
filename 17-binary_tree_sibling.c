6d1b252283527a1a62b41b44dc53eac59727cb37
AlexM4rcelli <6780@holbertonstudents.com>
2023-09-05 14:41:54 +0000

    Fix the condition of task 1 and 2

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -4,7 +4,8 @@
  * binary_tree_sibling - finds the sibling of a given node in a binary tree
  * @node: pointer to the node for which the sibling is to be found
  *
- * Return: a pointer to the sibling node, or NULL if the node or its parent is NULL
+ * Return: a pointer to the sibling node, or NULL if the node or its parent is
+ * NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,13 +9,13 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+    binary_tree_t *new_node = NULL;
 
-	new_node = binary_tree_node(parent, value);
-	if (!new_node)
-		return (NULL);
-	if (parent)
+    new_node = binary_tree_node(parent, value);
+    if (parent)
 	{
+		if (!new_node)
+        return (NULL);
 		if (parent->right)
 		{
 			new_node->right = parent->right;
@@ -23,5 +23,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		}
 		parent->right = new_node;
 	}
-	return (new_node);
+    return (new_node);
 }
