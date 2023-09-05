86f2ae53c14c472ba1154acc2e95ee5dcb418327
AlexM4rcelli <6780@holbertonstudents.com>
2023-09-05 13:53:31 +0000

    Adding Diego's code

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - calculates the depth of a binarytree
+ * @tree: pointer to the node to measure the depth
+ *
+ *  Return: the depth of a binary tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (-1);
+    else
+        return (1 + binary_tree_depth(tree->parent));
+}
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include"binary_trees.h"
+
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ *
+ * Return: the amount of leaves in a tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    size_t leftside = 0, rightside = 0;
+    if (!tree)
+        return (0);
+    if (!tree->left && !tree->right)
+        return (1);
+    leftside = binary_tree_leaves(tree->left);
+    rightside = binary_tree_leaves(tree->right);
+    return (leftside + rightside);
+}
